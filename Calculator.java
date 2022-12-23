41766b625973d2f0cc88e6a0a3f76cfb5337f5c7
Medhansh Shah <medhanshshah@gmail.com>
2022-12-23 19:12:44 +0530

    laid the foundation

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Calculator {
+    double degrees, minutes, value;
+    private String trigratio;
+    Scanner sc = new Scanner(System.in);
+    void input() {
+        System.out.print("Enter angle: ");
+        degrees = sc.nextDouble();
+        System.out.print("Enter minutes: ");
+        minutes = sc.nextDouble();
+        System.out.print("Enter trigonometric ratio: ");
+        trigratio = sc.next();
+        //System.out.println(trigratio);
+    }
+    void default() {
+        String rats[][] = new String[7][7];
+        
+    }
+    public static void main(String[] args) {
+        Calculator khs = new Calculator();
+        khs.input();
+    }
+}
